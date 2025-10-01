408e2091bcd8f92e8e18489882212da59f38a2a5
Timourka <timourkot@gmail.com>
2025-10-01 13:26:56 +0400

    5 laba

--- 5Laba/Program.fs
+++ 5Laba/Program.fs
@@ -1,45 +1,27 @@
-﻿open System
-open System.IO
+﻿open System.IO
 open System.Text.RegularExpressions
-open System.Collections.Concurrent
-open System.Threading.Tasks
 
-/// Функция для поиска самого частого слова в файле
-let mostFrequentWord (filePath: string) =
-    let text = File.ReadAllText(filePath).ToLower()
-    let words =
-        Regex.Matches(text, @"\w+")
-        |> Seq.cast<Match>
-        |> Seq.map (fun m -> m.Value)
+let mostCommonWord (path:string) =
+    let word = File.ReadAllText(path).ToLower()
+            |> fun text -> Regex.Split(text, @"\W+")  
+            |> Seq.filter (fun w -> w <> "")          
+            |> Seq.countBy id                         
+            |> Seq.maxBy snd                          
+            |> fst 
+    path, word
 
-    let grouped =
-        words
-        |> Seq.countBy id
-        |> Seq.sortByDescending snd
-        |> Seq.tryHead
+let inputDir = "./texts"
+let outputFile = "result.txt"
 
-    match grouped with
-    | Some (word, count) -> filePath, word, count
-    | None -> filePath, "[нет слов]", 0
+let files = Directory.GetFiles(inputDir, "*.txt")
 
-/// Главная функция
-[<EntryPoint>]
-let main argv =
-    // Входные файлы (можно указать свою папку)
-    let inputDir = "./texts"
-    let outputFile = "result.txt"
+let results = 
+    files
+    |> Array.Parallel.map mostCommonWord
 
-    let files = Directory.GetFiles(inputDir, "*.txt")
+let sw = new StreamWriter(outputFile, false)
+for (file, word) in results do
+    sw.WriteLine($"Файл: {Path.GetFileName(file)} | Слово: '{word}'")
+sw.Close()
 
-    // Параллельная обработка
-    let results = 
-        files
-        |> Array.Parallel.map mostFrequentWord
-
-    // Формируем выходной файл
-    use sw = new StreamWriter(outputFile, false)
-    for (file, word, count) in results do
-        sw.WriteLine($"Файл: {Path.GetFileName(file)} | Слово: '{word}' | Встречается: {count} раз")
-
-    printfn $"Результаты сохранены в {outputFile}"
-    0
+printfn $"Результаты сохранены в {outputFile}"
